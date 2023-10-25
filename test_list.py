988cc3486d41b41d5d8d7ed1fb1e3ed1995e5446
Student-Ayman <aymanbrahimi95@gmail.com>
2023-10-25 12:12:21 +0200

    test suppression de liste

--- test_list.py
+++ test_list.py
@@ -16,39 +16,59 @@ def creer_nouvelle_liste(nom_liste):
 
     with open(liste_path, "w") as fichier_liste:
         print(f"La liste '{nom_liste}' a été crée dans le répertoire 'liste' ! ")
-        
-#verifier existance fichier de la nvl liste
-class TestCreationListe(unittest.TestCase):
-    def test_verifier_fichier_cree(self):
-        nom_liste = "MaListe"
-        creer_nouvelle_liste(nom_liste)
-        liste_path = os.path.join("liste", f"{nom_liste}.txt")
-        self.assertTrue(os.path.exists(liste_path), f"Le fichier '{nom_liste}.txt' a été créé avec succès.")
-
 
 class TestCreationListe(unittest.TestCase):
     def test_creer_nouvelle_liste(self):
-
-
+        
         # Tester la création d'une nouvelle liste avec un nom valide
         with self.subTest(msg="Test avec un nom valide ! ! "):
             nom_liste = "MaListe"
             creer_nouvelle_liste(nom_liste)
             self.assertTrue(os.path.exists(f"liste/{nom_liste}.txt"))
         
-
         # Tester la création d'une nouvelle liste avec un nom existant
         with self.subTest(msg="Test avec un nom existant ! ! "):
             nom_liste = "MaListe"
             with self.assertRaises(ValueError):
                 creer_nouvelle_liste(nom_liste)
 
-                
         # Tester la création d'une nouvelle liste avec un nom vide
         with self.subTest(msg="Test avec un nom vide ! ! "):
             nom_liste = ""
             with self.assertRaises(ValueError):
                 creer_nouvelle_liste(nom_liste)
+
+
+
+def supprimer_liste(nom_liste):
+    # Verifier si le repertoire existe
+    liste_path = os.path.join("liste", f"{nom_liste}.txt")
+    if os.path.exists(liste_path):
+        os.remove(liste_path)
+        print(f"La liste '{nom_liste}' a été supprimée.")
+    else:
+        print(f"La liste '{nom_liste}' n'existe pas et ne peut pas être supprimée.")
+
+class TestSupprimerListe(unittest.TestCase):
+    def test_supprimer_liste(self):
+
+        with self.subTest(msg="Test Suppression avec succes ! ! "):
+            # Crée un fichier de liste de test
+            nom_liste = "MaListe"
+            test_liste_path = os.path.join("liste", f"{nom_liste}.txt")
+            
+            # Utilisez la fonction supprimer_liste pour supprimer le fichier de test
+            supprimer_liste(nom_liste)
+
+            # Vérifiez que le fichier a été supprimé
+            self.assertFalse(os.path.exists(test_liste_path))
+
+        with self.subTest(msg="Test Suppression de fichier inexistant"):
+            liste_inexistant = "inexistant"
+
+            # Utiliser la fonction supprimer_liste pour essayer de supprimer un fichier qui n'existe pas
+            with self.assertRaises(ValueError):
+                supprimer_liste(liste_inexistant)
 
 
 if __name__ == "__main__":
